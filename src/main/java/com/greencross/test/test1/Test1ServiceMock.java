4460f70f947d8afa452228ae411da366cee2ba3c
변상재 <sayaya@greencross.com>
2021-01-05 16:06:24 +0900

    Task ID 수정

--- src/main/java/com/greencross/test/test1/Test1ServiceMock.java
+++ src/main/java/com/greencross/test/test1/Test1ServiceMock.java
@@ -8,11 +8,11 @@ import java.util.List;
 @Service
 public class Test1ServiceMock {
 	private final static List<Task> tasks = List.of(
-			Task.builder().id(00000001L).name("Task1").dateRequest("2020-12-08").dateDue("2021-01-10").requester("User1").tags(new String[]{""}).description("%%%% 문서 정리")
+			Task.builder().id(1).name("Task1").dateRequest("2020-12-08").dateDue("2021-01-10").requester("User1").tags(new String[]{""}).description("%%%% 문서 정리")
 				.tags(new String[]{"Documentation"}).build(),
-			Task.builder().id(00010001L).name("Task2").dateRequest("2020-12-09").dateDue("2021-01-15").requester("User2").description("XXX가 YYYY 하는 버그 수정")
+			Task.builder().id(93940501L).name("Task2").dateRequest("2020-12-09").dateDue("2021-01-15").requester("User2").description("XXX가 YYYY 하는 버그 수정")
 				.tags(new String[]{"Debug", "Test"}).build(),
-			Task.builder().id(01000001L).name("Task3").dateRequest("2020-12-10").dateDue("2021-01-11").requester("User2").description("$##$ 기능 요청")
+			Task.builder().id(29000001L).name("Task3").dateRequest("2020-12-10").dateDue("2021-01-11").requester("User2").description("$##$ 기능 요청")
 				.tags(new String[]{"Development", "Documentation"}).build(),
 			Task.builder().id(10000001L).name("Task4").dateRequest("2020-12-11").dateDue("2021-01-12").requester("Manager1").description("$$$$ **** 기능 개발")
 				.tags(new String[]{"Development"}).build(),
